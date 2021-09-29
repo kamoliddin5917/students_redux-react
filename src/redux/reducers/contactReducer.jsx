8afef9457eddedb3b9d68c71a3e74db42afdbc28
toxir-brat <toxirli94@gmail.com>
2021-09-29 14:20:45 +0500

    base yes

--- src/redux/reducers/contactReducer.jsx
+++ src/redux/reducers/contactReducer.jsx
@@ -1,34 +1,26 @@
-const initialStore = [
-  {
-    id: 0,
-    name: "Kamoliddin Jamoliddinov",
-    email: "k@g.com",
-    number: 1234567,
-  },
-  {
-    id: 1,
-    name: "Umida Hanimqulova",
-    email: "u@g.com",
-    number: 7654321,
-  },
-];
+const initialStore = window.localStorage.getItem("base")
+  ? JSON.parse(window.localStorage.getItem("base"))
+  : [];
 
 const contactReducer = (state = initialStore, action) => {
   switch (action.type) {
     case "ADD_CONTACT":
       state = [...state, action.payload];
+      window.localStorage.setItem("base", JSON.stringify(state));
       return state;
     case "UPDATE_CONTACT":
       const updateState = state.map((contact) =>
         contact.id === action.payload.id ? action.payload : contact
       );
       state = updateState;
+      window.localStorage.setItem("base", JSON.stringify(state));
       return state;
     case "DELETE_CONTACT":
       const filterContacts = state.filter(
         (contact) => contact.id !== action.payload && contact
       );
       state = filterContacts;
+      window.localStorage.setItem("base", JSON.stringify(state));
       return state;
     default:
       return state;
